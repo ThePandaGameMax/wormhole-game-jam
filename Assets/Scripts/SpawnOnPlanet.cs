5c1b6a459038c82cd07e5d45d589ad72027d3db2
zooyl <natoniewski.m@gmail.com>
2020-02-17 16:07:20 +0100

    test commit

--- Assets/Scripts/SpawnOnPlanet.cs
+++ Assets/Scripts/SpawnOnPlanet.cs
@@ -18,7 +18,7 @@ public class SpawnOnPlanet : MonoBehaviour
         Quaternion spawnRotation = Quaternion.identity;
         GameObject newCharacter = Instantiate(character, spawnPosition, spawnRotation) as GameObject;
         newCharacter.transform.LookAt(planet.transform);
-        newCharacter.transform.Rotate(-90, 0, 0);
+        // newCharacter.transform.Rotate(-90, 0, 0);
         newCharacter.AddComponent<FauxGravityBody>();
         newCharacter.GetComponent<FauxGravityBody>().attractor = planet.GetComponent<FauxGravityAttractor>();
     }
