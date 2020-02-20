177eada0c2d8d2dc908e3dfde46de43c9e5b2cd8
ThePandaGameMax <inconue1236@gmail.com>
2020-02-20 11:00:13 +0400

    UI
    Created a Basic UI for the game

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void Onclicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+}
